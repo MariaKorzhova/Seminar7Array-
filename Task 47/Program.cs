d03602079f9200a71ca5ed6e848a372133b9dcd1
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-21 17:43:23 +0300

    Task 47

---
+++ Task 47/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите размер массив m x n");
+int m = inputNumber("m = ");
+int n = inputNumber("n = ");
+double[,] array = new double[m,n];
+Random rand = new Random();
+
+for(int i = 0; i < m; i++)
+{
+for(int j = 0; j < n; j++)
+{
+array[i,j] = Math.Round(rand.Next(-10, 10) + rand.NextDouble(), 3);
+System.Console.Write(array[i,j] + "\t");
+}
+System.Console.WriteLine();
+}
+
+int inputNumber(string str)
+{
+int number;
+string text;
+
+while (true)
+{
+System.Console.Write(str);
+text = Console.ReadLine();
+if (int.TryParse(text, out number))
+{
+break;
+}
+System.Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+}
+return number;
+}
+
+
