1348eace2d5fa97224deb2f04e5d308daee61513
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-21 17:56:09 +0300

    Task 47

--- Task 47/Program.cs
+++ Task 47/Program.cs
@@ -2,18 +2,27 @@
 System.Console.WriteLine("Введите размер массив m x n");
 int m = inputNumber("m = ");
 int n = inputNumber("n = ");
-double[,] array = new double[m,n];
-Random rand = new Random();
+double[,] NewArray = FillArrayAndPrint(m,n);
 
-for(int i = 0; i < m; i++)
+
+double[,] FillArrayAndPrint(int m, int n)
 {
-for(int j = 0; j < n; j++)
-{
-array[i,j] = Math.Round(rand.Next(-10, 10) + rand.NextDouble(), 3);
-System.Console.Write(array[i,j] + "\t");
+    double[,] array = new double[m,n];
+    Random rand = new Random();
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round(rand.Next(-10, 10) + rand.NextDouble(), 3);
+            System.Console.Write(array[i,j] + "\t");
+        }
+    System.Console.WriteLine();
+    }
+    return array;
 }
-System.Console.WriteLine();
-}
+
+
 
 int inputNumber(string str)
 {
